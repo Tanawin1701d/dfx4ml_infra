200eedd2ae544e02d67ea10a20941062b8a61142
tanawin <tanawin1701d@outlook.com>
2026-03-25 21:59:14 +0700

    add initial KV260 project setup including scripts, block designs, and Vivado project files

--- ip_src/dfx_unified/dfx_unified.tcl
+++ ip_src/dfx_unified/dfx_unified.tcl
@@ -1,169 +1,3 @@
-
-################################################################
-# This is a generated script based on design: dfx_wrapper
-#
-# Though there are limitations about the generated script,
-# the main purpose of this utility is to make learning
-# IP Integrator Tcl commands easier.
-################################################################
-
-namespace eval _tcl {
-proc get_script_folder {} {
-   set script_path [file normalize [info script]]
-   set script_folder [file dirname $script_path]
-   return $script_folder
-}
-}
-variable script_folder
-set script_folder [_tcl::get_script_folder]
-
-################################################################
-# Check if script is running in correct Vivado version.
-################################################################
-set scripts_vivado_version 2023.2
-set current_vivado_version [version -short]
-
-if { [string first $scripts_vivado_version $current_vivado_version] == -1 } {
-   puts ""
-   if { [string compare $scripts_vivado_version $current_vivado_version] > 0 } {
-      catch {common::send_gid_msg -ssname BD::TCL -id 2042 -severity "ERROR" " This script was generated using Vivado <$scripts_vivado_version> and is being run in <$current_vivado_version> of Vivado. Sourcing the script failed since it was created with a future version of Vivado."}
-
-   } else {
-     catch {common::send_gid_msg -ssname BD::TCL -id 2041 -severity "ERROR" "This script was generated using Vivado <$scripts_vivado_version> and is being run in <$current_vivado_version> of Vivado. Please run the script in Vivado <$scripts_vivado_version> then open the design in Vivado <$current_vivado_version>. Upgrade the design by running \"Tools => Report => Report IP Status...\", then run write_bd_tcl to create an updated script."}
-
-   }
-
-   return 1
-}
-
-################################################################
-# START
-################################################################
-
-# To test this script, run the following commands from Vivado Tcl console:
-# source dfx_wrapper_script.tcl
-
-# If there is no project opened, this script will create a
-# project, but make sure you do not have an existing project
-# <./myproj/project_1.xpr> in the current working folder.
-
-set list_projs [get_projects -quiet]
-if { $list_projs eq "" } {
-   create_project project_1 myproj -part xck26-sfvc784-2LV-c
-   set_property BOARD_PART xilinx.com:kv260_som_som240_1_connector_kv260_carrier_som240_1_connector:part0:1.4 [current_project]
-}
-
-
-# CHANGE DESIGN NAME HERE
-variable design_name
-set design_name dfx_wrapper
-
-# If you do not already have an existing IP Integrator design open,
-# you can create a design using the following command:
-#    create_bd_design $design_name
-
-# Creating design if needed
-set errMsg ""
-set nRet 0
-
-set cur_design [current_bd_design -quiet]
-set list_cells [get_bd_cells -quiet]
-
-if { ${design_name} eq "" } {
-   # USE CASES:
-   #    1) Design_name not set
-
-   set errMsg "Please set the variable <design_name> to a non-empty value."
-   set nRet 1
-
-} elseif { ${cur_design} ne "" && ${list_cells} eq "" } {
-   # USE CASES:
-   #    2): Current design opened AND is empty AND names same.
-   #    3): Current design opened AND is empty AND names diff; design_name NOT in project.
-   #    4): Current design opened AND is empty AND names diff; design_name exists in project.
-
-   if { $cur_design ne $design_name } {
-      common::send_gid_msg -ssname BD::TCL -id 2001 -severity "INFO" "Changing value of <design_name> from <$design_name> to <$cur_design> since current design is empty."
-      set design_name [get_property NAME $cur_design]
-   }
-   common::send_gid_msg -ssname BD::TCL -id 2002 -severity "INFO" "Constructing design in IPI design <$cur_design>..."
-
-} elseif { ${cur_design} ne "" && $list_cells ne "" && $cur_design eq $design_name } {
-   # USE CASES:
-   #    5) Current design opened AND has components AND same names.
-
-   set errMsg "Design <$design_name> already exists in your project, please set the variable <design_name> to another value."
-   set nRet 1
-} elseif { [get_files -quiet ${design_name}.bd] ne "" } {
-   # USE CASES: 
-   #    6) Current opened design, has components, but diff names, design_name exists in project.
-   #    7) No opened design, design_name exists in project.
-
-   set errMsg "Design <$design_name> already exists in your project, please set the variable <design_name> to another value."
-   set nRet 2
-
-} else {
-   # USE CASES:
-   #    8) No opened design, design_name not in project.
-   #    9) Current opened design, has components, but diff names, design_name not in project.
-
-   common::send_gid_msg -ssname BD::TCL -id 2003 -severity "INFO" "Currently there is no design <$design_name> in project, so creating one..."
-
-   create_bd_design $design_name
-
-   common::send_gid_msg -ssname BD::TCL -id 2004 -severity "INFO" "Making design <$design_name> as current_bd_design."
-   current_bd_design $design_name
-
-}
-
-common::send_gid_msg -ssname BD::TCL -id 2005 -severity "INFO" "Currently the variable <design_name> is equal to \"$design_name\"."
-
-if { $nRet != 0 } {
-   catch {common::send_gid_msg -ssname BD::TCL -id 2006 -severity "ERROR" $errMsg}
-   return $nRet
-}
-
-set bCheckIPsPassed 1
-##################################################################
-# CHECK IPs
-##################################################################
-set bCheckIPs 1
-if { $bCheckIPs == 1 } {
-   set list_check_ips "\ 
-user.org:user:DFX_Ctrl:1.0\
-user.org:user:Dfx_Streamer:1.0\
-xilinx.com:ip:xlconcat:2.1\
-xilinx.com:ip:xlconstant:1.1\
-xilinx.com:ip:dfx_controller:1.0\
-xilinx.com:ip:util_vector_logic:2.0\
-xilinx.com:ip:axi_gpio:2.0\
-user.org:user:icapWrap:1.0\
-xilinx.com:ip:axi_dma:7.1\
-xilinx.com:ip:dfx_decoupler:1.0\
-"
-
-   set list_ips_missing ""
-   common::send_gid_msg -ssname BD::TCL -id 2011 -severity "INFO" "Checking if the following IPs exist in the project's IP catalog: $list_check_ips ."
-
-   foreach ip_vlnv $list_check_ips {
-      set ip_obj [get_ipdefs -all $ip_vlnv]
-      if { $ip_obj eq "" } {
-         lappend list_ips_missing $ip_vlnv
-      }
-   }
-
-   if { $list_ips_missing ne "" } {
-      catch {common::send_gid_msg -ssname BD::TCL -id 2012 -severity "ERROR" "The following IPs are not found in the IP Catalog:\n  $list_ips_missing\n\nResolution: Please add the repository containing the IP(s) to the project." }
-      set bCheckIPsPassed 0
-   }
-
-}
-
-if { $bCheckIPsPassed != 1 } {
-  common::send_gid_msg -ssname BD::TCL -id 2023 -severity "WARNING" "Will not continue with creation of design due to the error(s) above."
-  return 3
-}
-
 ##################################################################
 # DESIGN PROCs
 ##################################################################
@@ -281,8 +115,8 @@ proc create_root_design { parentCell slot_index_width interface_widths applied_i
   }
 
   # 1. slot_index_width must be power of two
-  if { !($slot_index_width != 0 && ($slot_index_width & ($slot_index_width - 1)) == 0) } {
-     catch {common::send_gid_msg -ssname BD::TCL -id 2092 -severity "ERROR" "slot_index_width <$slot_index_width> is not a power of two!"}
+  if { $slot_index_width == 0 } {
+     catch {common::send_gid_msg -ssname BD::TCL -id 2092 -severity "ERROR" "slot_index_width <$slot_index_width> is zero"}
      return
   }
 
@@ -294,8 +128,8 @@ proc create_root_design { parentCell slot_index_width interface_widths applied_i
   }
 
   for {set i 0} {$i < [llength $interface_widths]} {incr i} {
-     set iw  [lindex $interface_widths $i]         # iw      interface width
-     set aiw [lindex $applied_interface_widths $i] # aiw     applied interface width
+     set iw  [lindex $interface_widths $i]         ;# iw      interface width
+     set aiw [lindex $applied_interface_widths $i] ;# aiw     applied interface width
 
      if { !($iw != 0 && ($iw & ($iw - 1)) == 0) } {
         catch {common::send_gid_msg -ssname BD::TCL -id 2094 -severity "ERROR" "interface_widths\[$i\] = <$iw> is not a power of two!"}
@@ -680,16 +514,4 @@ rm7 BS {0 {ID 0 ADDR 0 SIZE 0 CLEAR 0}} SHUTDOWN_REQUIRED hw RESET_REQUIRED low}
   validate_bd_design
   save_bd_design
 }
-# End of create_root_design()
-
-
-##################################################################
-# MAIN FLOW
-##################################################################
-
-
-common::send_gid_msg -ssname BD::TCL -id 2052 -severity "CRITICAL WARNING" "This Tcl script was generated from a block design that is out-of-date/locked. It is possible that design <$design_name> may result in errors during construction."
-
-# create_root_design ""
-
 
---
+++ ip_src/dfx_unified/dfx_unified_ip_composer.tcl
@@ -0,0 +1,41 @@
+# Vivado script to create project for KV260 board and call create_root_design
+
+# Set project parameters
+set project_name "kv260_dfx_project"
+set project_dir "./kv260_dfx_project"
+set bd_name "design_1"
+set part "xck26-sfvc784-2LV-c"
+set board_part "xilinx.com:kv260_som:part0:1.4"
+
+# Create project
+create_project $project_name $project_dir -part $part -force
+set_property board_part $board_part [current_project]
+
+# Add IP repository
+set project_root [file normalize [file join [file dirname [info script]] ../../ip_repo]]
+set_property  ip_repo_paths  [list [file join $project_root ip_repo]] [current_project]
+update_ip_catalog
+
+# Create block design
+create_bd_design $bd_name
+
+# Source the design script
+source [file join [file dirname [info script]] dfx_unified.tcl]
+
+# Define arguments for create_root_design
+# These values are example parameters; adjust them according to your specific needs.
+set parentCell ""
+set slot_index_width 3 ;# Must be a power of two
+set interface_widths [list 32 32 32 32 32 32 32 32] ;# Must be power of two
+set applied_interface_widths [list 32 32 32 32 32 32 32 32] ;# Must be <= interface_widths
+
+# Call create_root_design
+create_root_design $parentCell $slot_index_width $interface_widths $applied_interface_widths
+
+# Finalize block design
+save_bd_design
+validate_bd_design
+make_wrapper -files [get_files ${project_dir}/${project_name}.srcs/sources_1/bd/${bd_name}/${bd_name}.bd] -top
+add_files -norecurse ${project_dir}/${project_name}.gen/sources_1/bd/${bd_name}/hdl/${bd_name}_wrapper.v
+
+puts "Project $project_name created successfully with KV260 board support."
